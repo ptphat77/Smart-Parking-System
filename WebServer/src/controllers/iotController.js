07fd8d63c877f227b016fe47dce150283ef06704
ptphat77 <pptphat77@gmail.com>
2023-11-28 16:43:04 +0700

    check error from iot

--- WebServer/src/controllers/iotController.js
+++ WebServer/src/controllers/iotController.js
@@ -3,9 +3,11 @@ const slotService = require('../services/slotService');
 const getDataIoT = async (req, res) => {
     if (req.body.errorCode == 0) {
         await slotService.updateSlotList(req.body.data);
+        console.log(req.body.data);
+        return res.status(200).json({ message: 'Success!!!' });
+    } else {
+        return res.status(400).json({ message: 'Failed!!!' });
     }
-
-    return res.status(200).json(req.body);
 };
 
 module.exports = {
