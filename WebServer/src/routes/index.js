c91011f5d89b7060df08e6e80e24e7581a244360
Ter-Rung <20522075@gm.uit.edu.vn>
2023-12-02 23:54:21 +0700

    login feature and modify frontend

--- WebServer/src/routes/index.js
+++ WebServer/src/routes/index.js
@@ -2,12 +2,14 @@ const homeController = require('../controllers/homeController');
 const iotController = require('../controllers/iotController');
 const webController = require('../controllers/webController');
 const userController = require('../controllers/userController');
+const loginController = require('../controllers/loginController');
 
 function route(app) {
     app.get('/register', webController.handleRegister);
     app.get('/login', webController.handleLogin);
 
     app.post('/register', userController.createFunc);
+    app.post('/login', loginController.checkUserFunc);
 
     app.post('/iot', iotController.updateFunc);
     app.get('/iot', iotController.readFunc);
