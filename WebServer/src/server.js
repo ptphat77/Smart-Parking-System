02ff6dde1f00f33c426f1e006c0d76e821a50cb6
ptphat77 <pptphat77@gmail.com>
2023-12-06 01:12:17 +0700

    config cors

--- WebServer/src/server.js
+++ WebServer/src/server.js
@@ -9,6 +9,7 @@ const cookieParser = require ('cookie-parser');
 const route = require('./routes');
 const viewEngine = require('./config/viewEngine');
 const db = require('./config/db');
+import configCors from './config/cors';
 
 // Connect to db
 db.connect();
@@ -17,6 +18,16 @@ const app = express();
 const server = http.createServer(app);
 export const io = new Server(server);
 const port = process.env.PORT;
+
+// config Cors
+// configCors(app);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.0.102:5573'); // Change this to your allowed origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 
 // Config view engine
 viewEngine.config(app);
